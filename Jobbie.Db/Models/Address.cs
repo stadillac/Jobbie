8d3f02b19ad140c2ae53568f184a19b8aee3e07c
stadillac <christopherstalder@outlook.com>
2023-09-17 10:47:27 -0400

    Adding address entity

---
+++ Jobbie.Db/Models/Address.cs
@@ -0,0 +1,13 @@
+﻿namespace Jobbie.Db.Models
+{
+    public class Address : Audit
+    {
+        public string Address1 { get; set; } = string.Empty;
+        public string Address2 { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+        public int StateId { get; set; }
+        public string ZipCode { get; set; } = string.Empty;
+
+        public virtual State State { get; set; } = new();
+    }
+}
