1539cab852f8ce6f9ff1a22e274e69127cdff151
stadillac <christopherstalder@outlook.com>
2023-09-18 16:22:59 -0400

    Adding bootstrap, jquery, and automapper. Adding some custom js for future use

--- Jobbie.Web/wwwroot/js/site.js
+++ Jobbie.Web/wwwroot/js/site.js
@@ -1,4 +1,59 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿function modalPopup(editUrl, itemId) {
+    var options = { "backdrop": "static", keyboard: true };
+    $.ajax({
+        type: "GET",
+        url: editUrl,
+        contentType: "application/json; charset=utf-8",
+        data: { "Id": itemId },
+        datatype: "json",
+        success: function (data) {
+            $('#myModalContent').html(data);
+            $('#myModal').modal(options);
+            $('#myModal').modal('show');
+        },
+        error: function () {
+            alert("Dynamic content load failed.");
+        }
+    });
+}
 
-// Write your JavaScript code.
+function softDelete(targetUrl, targetId, elem) {
+    var row = elem.closest('tr');
+    var obj = $(elem).parent().parent();
+
+    $.ajax({
+        type: "POST",
+        url: targetUrl,
+        data: { id: targetId },
+        success: function (result) {
+
+            $('#successful-toast').toast('show');
+
+            obj.fadeTo(10, 1, () => {
+                obj.children('td', 'th')
+                    .animate({ 'padding-top': '0', 'padding-bottom': '0' })
+                    .wrapInner('<div />')
+                    .children()
+                    .slideUp(100, () => { obj.remove(); });
+            });
+        },
+        error: function (data) {
+            $('#failed-toast').toast('show');
+        }
+    });
+}
+
+$('.dropdown-menu a.dropdown-toggle').on('click', function (e) {
+    if (!$(this).next().hasClass('show')) {
+        $(this).parents('.dropdown-menu').first().find('.show').removeClass("show");
+    }
+    var $subMenu = $(this).next(".dropdown-menu");
+    $subMenu.toggleClass('show');
+
+
+    $(this).parents('li.nav-item.dropdown.show').on('hidden.bs.dropdown', function (e) {
+        $('.dropdown-submenu .show').removeClass("show");
+    });
+
+    return false;
+});
