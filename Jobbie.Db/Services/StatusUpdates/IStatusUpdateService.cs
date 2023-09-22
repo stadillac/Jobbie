8d22aa1efd334e6b453e1da801022968520302d6
stadillac <christopherstalder@outlook.com>
2023-09-22 14:28:13 -0400

    Adding status update service

---
+++ Jobbie.Db/Services/StatusUpdates/IStatusUpdateService.cs
@@ -0,0 +1,8 @@
+﻿using Jobbie.Db.Models;
+
+namespace Jobbie.Db.Services
+{
+    public interface IStatusUpdateService : IBaseService<StatusUpdate>
+    {
+    }
+}
---
+++ Jobbie.Db/Services/StatusUpdates/StatusUpdateService.cs
@@ -0,0 +1,12 @@
+﻿using Jobbie.Db.Models;
+
+namespace Jobbie.Db.Services
+{
+    /// <inheritdoc />
+    public class StatusUpdateService : BaseService<StatusUpdate>, IStatusUpdateService
+    {
+        public StatusUpdateService(ApplicationContext context) : base(context)
+        {
+        }
+    }
+}
